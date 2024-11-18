f4538db5d78bcb4e2f80a40640376b99c1be51ca
juyeong.park <qkqwnjdud@gmail.com>
2024-11-18 11:34:42 +0900

    electron 초기 설정 추가

--- public/electron.js
+++ public/electron.js
@@ -3,30 +3,23 @@ const path = require('path');
 const url = require('url');
 
 function createWindow() {
-    /*
-    * 넓이 1920에 높이 1080의 FHD 풀스크린 앱을 실행시킵니다.
-    * */
+    
+    // 브라우저 창을 생성합니다.
     const win = new BrowserWindow({
-        width:800,
-        height:600
+        width:1280,
+        height:800,
+        icon : path.join(__dirname, 'assets/logo.png'),
     });
 
-    /*
-    * ELECTRON_START_URL을 직접 제공할경우 해당 URL을 로드합니다.
-    * 만일 URL을 따로 지정하지 않을경우 (프로덕션빌드) React 앱이
-    * 빌드되는 build 폴더의 index.html 파일을 로드합니다.
-    * */
+    
     const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
         protocol: 'file:',
-        slashes: true
+        slashes: true,
     });
 
-    /*
-    * startUrl에 배정되는 url을 맨 위에서 생성한 BrowserWindow에서 실행시킵니다.
-    * */
+    // 초기 로드할 웹사이트의 주소를 지정합니다.
     win.loadURL(startUrl);
-
 }
 
 app.on('ready', createWindow);
